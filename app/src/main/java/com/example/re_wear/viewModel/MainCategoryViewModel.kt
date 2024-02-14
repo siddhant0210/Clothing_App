03210806fa72ac337ea9fecf799d66ec18277b64
Siddhant Dwivedi <siddhant.dwivedi@innobitsystems.com>
2024-02-14 17:51:27 +0530

    sid commit

--- app/src/main/java/com/example/re_wear/viewModel/MainCategoryViewModel.kt
+++ app/src/main/java/com/example/re_wear/viewModel/MainCategoryViewModel.kt
@@ -1,10 +1,23 @@
 package com.example.re_wear.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.bumptech.glide.load.engine.Resource
+import com.example.re_wear.data.Product
+import com.google.common.io.Resources
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseCommonRegistrar
 import com.google.firebase.FirebaseException
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class MainCategoryViewModel @Inject constructor(
+    private var firebase : Firebase
 ): ViewModel() {
+
+    private val _specialProducts = MutableStateFlow<com.example.re_wear.util.Resource<List<Product>>>(com.example.re_wear.util.Resource.UnSpecified(""))
+    val  specialProduct:StateFlow<com.example.re_wear.util.Resource<List<Product>>> = _specialProducts
+
+
 }
